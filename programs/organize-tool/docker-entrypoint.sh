1d5d1ff60e8164952bbfe5e8cf583e1d543c2196
zorbaTheRainy <github2@scout-family.org>
2025-11-29 23:36:23 +0100

    typo

--- programs/organize-tool/docker-entrypoint.sh
+++ programs/organize-tool/docker-entrypoint.sh
@@ -4,9 +4,9 @@ set -e
 # If a crontab file is mounted at /etc/cron.d/organize, install it
 if [ -f /etc/cron.d/organize-tool-crontab ]; then
   echo "Installing crontab from /etc/cron.d/organize..."
-  cp /etc/cron.d/organize /etc/cron.d/organize
-  chmod 0644 /etc/cron.d/organize
-  crontab /etc/cron.d/organize
+  # cp /etc/cron.d/organize /etc/cron.d/organize
+  chmod 0644 /etc/cron.d/organize-tool-crontab 
+  crontab /etc/cron.d/organize-tool-crontab 
 fi
 
 # Ensure cron logs go to stdout
